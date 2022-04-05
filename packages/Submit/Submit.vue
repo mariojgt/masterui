e8ee5101019995430d24cb073debaca433944c11
mario tarosso <mariojgt2@gmail.com>
2022-04-05 22:36:44 +0100

    button update

--- packages/Submit/Submit.vue
+++ packages/Submit/Submit.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="form-control pt-6">
-        <button class="btn btn-primary">
-            <span class="inline-block mr-2">{{ props.name }}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                class="w-4 h-4 inline-block">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        <button class="btn btn-primary btn-block space-x-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
             </svg>
+
+            <span>{{ props.name }}</span>
         </button>
     </div>
 </template>
