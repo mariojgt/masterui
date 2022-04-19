5910e6c1b72faa7686b343bbcc0034465c5b750b
mario tarosso <mariojgt2@gmail.com>
2022-04-19 20:40:25 +0100

    up

--- packages/index.js
+++ packages/index.js
@@ -1,5 +1,5 @@
 // Export the vue components
-export { default as Input } from "./Input/Input.vue";
-export { default as Link } from "./Input/Link.vue";
-export { default as Password } from "./Input/Password.vue";
-export { default as Submit } from "./Input/Submit.vue";
+export { default as InputField } from "./Input/Input.vue";
+export { default as LinkButton } from "./Link/Link.vue";
+export { default as InputPassword } from "./Password/Password.vue";
+export { default as Submit } from "./Submit/Submit.vue";
