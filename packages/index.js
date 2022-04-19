6c7a0fd582c412f919c83b9b104aeb8f28b94973
mario jose goes tarosso <mariojgt2@gmail.com>
2022-04-19 14:35:43 +0100

    export fix

---
+++ packages/index.js
@@ -0,0 +1,5 @@
+// Export the vue components
+export { default as Input } from "./Input/Input.vue";
+export { default as Link } from "./Input/Link.vue";
+export { default as Password } from "./Input/Password.vue";
+export { default as Submit } from "./Input/Submit.vue";
