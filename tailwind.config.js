dd3b7c0302ae14c2f7f460c12c68bd8ff8129dc8
mario jose goes tarosso <mariojgt2@gmail.com>
2022-04-20 10:19:45 +0100

    node module ignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: { extend: {} },
-    plugins: [require("daisyui")],
-};
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
