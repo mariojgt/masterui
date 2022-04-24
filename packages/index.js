59db23a8743f812d5e59ff3ad2494cf71eb2257b
mario tarosso <mariojgt2@gmail.com>
2022-04-24 22:02:40 +0100

    select field

--- packages/index.js
+++ packages/index.js
@@ -3,3 +3,4 @@ export { default as InputField } from "./Input/Input.vue";
 export { default as LinkButton } from "./Link/Link.vue";
 export { default as InputPassword } from "./Password/Password.vue";
 export { default as Submit } from "./Submit/Submit.vue";
+export { default as SelectInput } from "./Select/Select.vue";
