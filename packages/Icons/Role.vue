d6dcf3d955583f2c5f382046248d9ba88bf0d6a6
mario tarosso <mariojgt@marios-mbp.home>
2022-05-06 23:14:57 +0100

    vue js mdi icon suport added

---
+++ packages/Icons/Role.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg-icon type="mdi" :path="mdiLockOpen" :size="props.size"></svg-icon>
+</template>
+
+<script setup>
+// Import the mdi icon component
+import SvgIcon from '@jamescoyle/vue-icon'
+// Import the icon you want to use
+import { mdiLockOpen } from '@mdi/js'
+
+// Page props
+const props = defineProps({
+    size: {
+        type: Number,
+        default: 25,
+    },
+});
+
+</script>
---
+++ packages/Icons/index.js
@@ -0,0 +1,6 @@
+// Export the vue components icon note that the name must chat in the database or else will not worko
+export { default as home } from "./Home.vue";
+export { default as role } from "./Role.vue";
+export { default as permission } from "./Permission.vue";
+export { default as navigation } from "./Navigation.vue";
+
