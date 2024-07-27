0deb6a0611167779bdf5af7b02044f406885196b
Mario Tarosso <mariojgt2@gmail.com>
2024-07-27 15:02:49 +0000

    ui updates

--- packages/Image/Image.vue
+++ packages/Image/Image.vue
@@ -22,85 +22,73 @@
             <strong>{{ errorMessage }}</strong>
         </span>
 
-        <div class="mt-3 bg-white rounded" v-if="availableSearchItems.data">
-            <div class="px-2 py-3 bg-success text-neutral rounded-t">
+        <div class="mt-3 bg-base-100 rounded" v-if="availableSearchItems.data[0]">
+            <div class="px-2 py-3 bg-primary text-white rounded-t">
                 <strong>Available Items</strong>
             </div>
-            <div
-                class="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4 border-l-transparent hover:rounded text-neutral hover:bg-green-700 hover:text-white"
-                v-for="(item, index) in availableSearchItems.data"
-                :key="item.id"
-                @click="addMedia(item)"
-            >
-                <div class="inline-flex items-center space-x-2">
-                    <div>
-                        <img
-                            :src="item.url.default"
-                            alt=""
-                            class="h-10 w-10 flex-shrink-0 rounded-full"
-                        />
-                    </div>
-                    <div>
-                        <strong>{{ item.name }}</strong>
-                    </div>
-                </div>
-                <div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M4.5 12.75l6 6 9-13.5"
-                        />
-                    </svg>
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2 bg-dark-100">
+                <div
+                    class="flex flex-col items-center border border-success p-3 hover:text-white rounded cursor-pointer"
+                    v-for="(item, index) in availableSearchItems.data"
+                    :key="item.id"
+                    @click="addMedia(item)"
+                >
+                    <img
+                        :src="item.url.default"
+                        class="h-60 w-auto mb-2"
+                    />
+                    <strong class="text-lg font-bold">{{ item.name }}</strong>
+                    <button class="btn btn-success mt-2 w-full">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mt-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
 
         <div class="mt-3 bg-base-300 rounded" v-if="selectedMedia.length > 0">
-            <!-- Add title here -->
-            <div class="px-2 py-3 bg-error text-neutral rounded-t">
+            <div class="px-2 py-3 bg-primary text-white rounded-t">
                 <strong>Selected Items</strong>
             </div>
-            <div
-                class="flex justify-between items-center border-b border-slate-200 text-neutral-content py-3 px-2 border-l-4 border-l-transparent hover:rounded hover:bg-red-700 hover:text-white"
-                v-for="(item, index) in selectedMedia"
-                :key="item.id"
-                @click="removeMedia(index)"
-            >
-                <div class="inline-flex items-center space-x-2">
-                    <div>
-                        <img
-                            :src="item.url.default"
-                            alt=""
-                            class="h-10 w-10 flex-shrink-0 rounded-full"
-                        />
-                    </div>
-                    <div>
-                        <strong>{{ item.name }}</strong>
-                    </div>
-                </div>
-                <div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                    </svg>
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-2">
+                <div
+                    class="flex flex-col items-center border border-error p-3 hover:text-white rounded cursor-pointer"
+                    v-for="(item, index) in selectedMedia"
+                    :key="item.id"
+                >
+                    <img
+                        :src="item.url.default"
+                        class="h-60 w-auto mb-2"
+                    />
+                    <strong class="text-lg font-bold">{{ item.name }}</strong>
+                    <button @click="removeMedia(index)" class="btn btn-error mt-2 w-full">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mt-2"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
@@ -108,69 +96,69 @@
 </template>
 
 <script setup lang="ts">
-    import { ref, watchEffect, defineProps, defineEmits } from "vue";
-    import axios from "axios";
+import { ref, watchEffect, defineProps, defineEmits } from "vue";
+import axios from "axios";
 
-    // Define types for media items
-    interface MediaItem {
-        id: number;
-        name: string;
-        url: {
-            default: string;
-        };
-    }
-
-    // Define component props
-    const props = defineProps<{
-        label: string;
-        name: string;
-        id: string;
-        placeholder: string;
-        modelValue: MediaItem[];
-        endpoint: string;
-        loadData?: MediaItem[]; // Optional prop for pre-loaded data
-    }>();
-
-    // Define component state
-    let searchString = ref<string>("");
-    let availableSearchItems = ref<{ data: MediaItem[] }>({ data: [] });
-    let selectedMedia = ref<MediaItem[]>(props.modelValue || []);
-    let errorMessage = ref<string | null>(null);
-
-    // Watch for changes in pre-loaded data
-    watchEffect(() => {
-        selectedMedia.value = props.loadData || [];
-    });
-
-    // Define the emit function for updating modelValue
-    const emit = defineEmits(["update:modelValue"]);
-
-    // Function to search for images
-    const searchImage = () => {
-        axios
-            .post<{ data: MediaItem[] }>(props.endpoint, {
-                search: searchString.value,
-            })
-            .then((response) => {
-                availableSearchItems.value = response.data;
-            })
-            .catch((error) => {
-                errorMessage.value = error.message;
-            });
+// Define types for media items
+interface MediaItem {
+    id: number;
+    name: string;
+    url: {
+        default: string;
     };
+}
 
-    // Function to add a media item
-    const addMedia = (item: MediaItem) => {
-        if (!selectedMedia.value.find((media) => media.id === item.id)) {
-            selectedMedia.value.push(item);
-            emit("update:modelValue", selectedMedia.value);
-        }
-        availableSearchItems.value = { data: [] };
-    };
+// Define component props
+const props = defineProps<{
+    label: string;
+    name: string;
+    id: string;
+    placeholder: string;
+    modelValue: MediaItem[];
+    endpoint: string;
+    loadData?: MediaItem[]; // Optional prop for pre-loaded data
+}>();
 
-    // Function to remove a media item
-    const removeMedia = (index: number) => {
-        selectedMedia.value.splice(index, 1);
+// Define component state
+let searchString = ref<string>("");
+let availableSearchItems = ref<{ data: MediaItem[] }>({ data: [] });
+let selectedMedia = ref<MediaItem[]>(props.modelValue || []);
+let errorMessage = ref<string | null>(null);
+
+// Watch for changes in pre-loaded data
+watchEffect(() => {
+    selectedMedia.value = props.loadData || [];
+});
+
+// Define the emit function for updating modelValue
+const emit = defineEmits(["update:modelValue"]);
+
+// Function to search for images
+const searchImage = () => {
+    axios
+        .post<{ data: MediaItem[] }>(props.endpoint, {
+            search: searchString.value,
+        })
+        .then((response) => {
+            availableSearchItems.value = response.data;
+        })
+        .catch((error) => {
+            errorMessage.value = error.message;
+        });
+};
+
+// Function to add a media item
+const addMedia = (item: MediaItem) => {
+    if (!selectedMedia.value.find((media) => media.id === item.id)) {
+        selectedMedia.value.push(item);
         emit("update:modelValue", selectedMedia.value);
-    };
+    }
+    availableSearchItems.value = { data: [] };
+};
+
+// Function to remove a media item
+const removeMedia = (index: number) => {
+    selectedMedia.value.splice(index, 1);
+    emit("update:modelValue", selectedMedia.value);
+};
 </script>
--- packages/MultipleText/TextMultipleSelector.vue
+++ packages/MultipleText/TextMultipleSelector.vue
@@ -4,45 +4,39 @@
             <span class="block text-lg font-bold mb-2">{{ props.label }}</span>
         </label>
 
-        <input class="input input-primary input-bordered" name="{{ props.name }}" id="{{ props.id }}"
+        <input class="input input-primary input-bordered" :name="props.name" :id="props.id"
             :placeholder="props.placeholder" autocomplete="off" v-model="searchString" @keyup="searchImage" />
 
         <span class="invalid-feedback text-primary" role="alert">
             <strong>{{ errorMessage }}</strong>
         </span>
-        <div class="mt-3 bg-white rounded" v-if="availableItems.data">
+        <div v-if="availableItems.data" class="mt-3 rounded p-1 bg-base-100">
             <!-- Add title here -->
-            <div class="px-2 py-3 bg-green-200 text-black rounded-t">
+            <div class="px-2 py-3 text-xl font-bold bg-primary text-white rounded mb-2 text-left">
                 <strong>Available Items</strong>
             </div>
-            <div class="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4 border-l-transparent hover:rounded text-black hover:bg-green-700 hover:text-white"
+            <div class="flex justify-between items-center text-black p-2 bg-white rounded-lg hover:bg-secondary border border-black mb-2"
                 v-for="(item, index) in availableItems.data" :key="item.id" @click="addItem(item)">
-                <div class="inline-flex items-center space-x-2">
-                    <div>
-                        <strong>{{ item[props.displayKey] }}</strong>
-                    </div>
-                </div>
-                <div>
+                <p class="text-2xl font-bold text-black">{{ item[props.displayKey] }}</p>
+                <div class="btn btn-success">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
+                        stroke="currentColor" class="w-6 h-6 text-white">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                     </svg>
                 </div>
             </div>
         </div>
-
         <!-- Show the selected items -->
-        <div class="mt-3 bg-white rounded">
-            <div class="flex justify-between items-center border-b border-slate-200 text-black py-3 px-2 border-l-4 border-l-transparent hover:rounded hover:bg-red-700 hover:text-white"
-                v-for="(item, index) in selectedItem" :key="item.id" @click="removeItem(index)">
-                <div class="inline-flex items-center space-x-2">
-                    <div>
-                        <strong>{{ item[props.displayKey] }}</strong>
-                    </div>
-                </div>
-                <div>
+        <div v-if="selectedItem.length > 0" class="mt-3 rounded p-1 bg-base-100">
+            <div class="px-2 py-3 text-xl bg-secondary text-white rounded mb-2 text-left">
+                <strong>Selected Items</strong>
+            </div>
+            <div class="flex justify-between items-center text-black p-2 bg-white rounded-lg hover:bg-secondary border border-black mb-2"
+                v-for="(item, index) in selectedItem" :key="item.id">
+                <p class="text-2xl font-bold text-black">{{ item[props.displayKey] }}</p>
+                <div class="btn btn-error" @click="removeItem(index)">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
+                        stroke="currentColor" class="w-6 h-6 text-white">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
@@ -53,9 +47,7 @@
 </template>
 
 <script setup>
-// Import axios
 import axios from "axios";
-
 import { watch, onMounted, defineEmits, ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
 
@@ -124,7 +116,7 @@ const update = (event) => {
 };
 
 let searchString = ref("");
-let availableItems = ref("");
+let availableItems = ref([]);
 const searchImage = async () => {
     axios
         .post(props.endpoint, {
@@ -146,12 +138,15 @@ const searchImage = async () => {
 
 let selectedItem = ref([]);
 
-const addItem = async (item) => {
+const addItem = (item) => {
     if (props.singleMode) {
         selectedItem.value = [];
     }
+    if (!Array.isArray(selectedItem.value)) {
+        selectedItem.value = [];
+    }
     // Add the image to the selected media make sure is unique value
-    if (selectedItem.value.indexOf(item) === -1) {
+    if (selectedItem.value.findIndex((i) => i.id === item.id) === -1) {
         selectedItem.value.push({
             id: item.id,
             [props.displayKey]: item[props.displayKey],
@@ -162,7 +157,7 @@ const addItem = async (item) => {
     emit("update:modelValue", selectedItem.value);
 };
 
-const removeItem = async (index) => {
+const removeItem = (index) => {
     selectedItem.value.splice(index, 1);
 
     emit("update:modelValue", selectedItem.value);
