ff518a261620dabb65d0e6c7dcec6dc9dcb2fd31
Mario Tarosso <mariojgt2@gmail.com>
2024-11-24 12:44:10 +0000

    mobile modal fix

--- packages/ReusableModal/ReusableModal.vue
+++ packages/ReusableModal/ReusableModal.vue
@@ -3,21 +3,29 @@
         <Dialog as="div" @close="emitClose" class="relative z-10">
             <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
                 leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-black bg-opacity-25" />
+                <div class="fixed inset-0 bg-black/25 sm:bg-black/25" />
             </TransitionChild>
 
             <div class="fixed inset-0 overflow-y-auto">
-                <div class="flex min-h-full items-center justify-center p-4 text-center">
-                    <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95">
+                <div class="flex min-h-full sm:items-center sm:justify-center sm:p-4 text-center">
+                    <TransitionChild as="template"
+                        enter="transform transition ease-out duration-300"
+                        enter-from="translate-y-full opacity-0 sm:scale-95 sm:translate-y-0"
+                        enter-to="translate-y-0 opacity-100 sm:scale-100"
+                        leave="transform transition ease-in duration-200"
+                        leave-from="translate-y-0 opacity-100 sm:scale-100"
+                        leave-to="translate-y-full opacity-0 sm:scale-95 sm:translate-y-0">
                         <DialogPanel
                         :class="[
-                            props.showModalBox ? 'modal-box' : '',
-                            { 'w-11/12 max-w-5xl': props.isLarge },
+                            'transform transition-all relative',
+                            props.showModalBox ? 'sm:modal-box' : 'p-5',
+                            { 'sm:w-11/12 sm:max-w-5xl': props.isLarge },
+                            'w-full min-h-screen sm:min-h-0 sm:w-auto',
+                            'sm:rounded-lg',
                             props.modalClass
                         ]">
-                            <button v-if="showHeader" @click="emitClose" :class="topCloseButtonClass">
+                            <button v-if="showHeader" @click="emitClose"
+                                class="absolute right-4 top-4">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     class="h-6 w-6"
@@ -31,20 +39,28 @@
                                     d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
-                            <slot v-if="showHeader" name="title">
-                                <h3 class="font-bold text-lg">Hello!</h3>
-                            </slot>
-                            <div class="mt-2">
-                                <slot name="body">
-                                    <p class="text-sm text-gray-500">
-                                        Your payment has been successfully submitted. We’ve sent you
-                                        an email with all of the details of your order.
-                                    </p>
+                            <div class="p-6 sm:p-0">
+                                <slot v-if="showHeader" name="title">
+                                    <h3 class="text-2xl font-semibold text-white sm:text-gray-900 dark:sm:text-white mb-8">
+                                        Sign in
+                                    </h3>
                                 </slot>
-                            </div>
-                            <div class="py-3 sm:flex sm:flex-row-reverse gap-4" v-if="showFooter">
-                                <button @click="emitConfirm" type="button" class="btn btn-success">{{ props.confirmText }}</button>
-                                <button @click="emitClose" type="button" class="btn btn-error">{{ props.cancelText }}</button>
+                                <div class="mt-2">
+                                    <slot name="body">
+                                        <p class="text-sm text-gray-500">
+                                            Your payment has been successfully submitted. We've sent you
+                                            an email with all of the details of your order.
+                                        </p>
+                                    </slot>
+                                </div>
+                                <div class="py-3 sm:flex sm:flex-row-reverse gap-4" v-if="showFooter">
+                                    <button @click="emitConfirm" type="button" class="btn btn-success w-full sm:w-auto">
+                                        {{ props.confirmText }}
+                                    </button>
+                                    <button @click="emitClose" type="button" class="btn btn-error w-full sm:w-auto">
+                                        {{ props.cancelText }}
+                                    </button>
+                                </div>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
@@ -65,9 +81,9 @@ import {
 } from '@headlessui/vue'
 
 const props = defineProps({
-    show: Boolean, // prop for controlling visibility of the modal
-    title: String, // prop for the title of the modal
-    content: String, // prop for the content of the modal
+    show: Boolean,
+    title: String,
+    content: String,
     cancelText : {
         type: String,
         default: 'Cancel'
@@ -86,7 +102,7 @@ const props = defineProps({
     },
     topCloseButtonClass : {
         type: String,
-        default: 'absolute top-1 right-1 p-4 bg-primary rounded-full hover:bg-base-300'
+        default: ''
     },
     showFooter : {
         type: Boolean,
@@ -105,10 +121,10 @@ const props = defineProps({
 const emit = defineEmits(['close', 'confirm'])
 
 const emitClose = () => {
-    emit('close') // emit the 'close' event when the modal is closed
+    emit('close')
 }
 
 const emitConfirm = () => {
-    emit('confirm') // emit the 'confirm' event when the modal is confirmed
+    emit('confirm')
 }
 </script>
